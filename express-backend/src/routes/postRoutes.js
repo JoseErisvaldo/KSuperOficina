8e5910ba6f715883e7ae26e91e06e1e2c20dacbf
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-30 22:15:53 -0300

    feat: message error 404

--- express-backend/src/routes/postRoutes.js
+++ express-backend/src/routes/postRoutes.js
@@ -20,6 +20,7 @@ router.get('/', async (req, res) => {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
+    res.status(404).json({ message: 'Error 404', error: error.message });
     res.status(500).json({ message: 'Internal Server Error', error: error.message });
   }
 });
