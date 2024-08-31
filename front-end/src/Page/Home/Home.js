28c54a24953b1e3d5e29ba7d02165bc5fe031cf8
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-31 16:33:37 -0300

    Feat: removing data from home

--- front-end/src/Page/Home/Home.js
+++ front-end/src/Page/Home/Home.js
@@ -30,82 +30,8 @@ export default function Home() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Painel de Administração de Catálogos</h1>
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
       
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button className="mb-4">
-             Adicionar Catálogo
-          </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Adicionar Novo Catálogo</DialogTitle>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            
-            <Input
-              placeholder="Nome do Catálogo"
-              value={newCatalog.name}
-              onChange={(e) => setNewCatalog({ ...newCatalog, name: e.target.value })}
-            />
-            <Input
-              placeholder="Descrição"
-              value={newCatalog.description}
-              onChange={(e) => setNewCatalog({ ...newCatalog, description: e.target.value })}
-            />
-            <Button onClick={addCatalog}>Adicionar</Button>
-          </div>
-        </DialogContent>
-      </Dialog>
-
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Nome</TableHead>
-            <TableHead>Descrição</TableHead>
-            <TableHead>Ações</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {catalogs.map((catalog) => (
-            <TableRow key={catalog.id}>
-              <TableCell>{catalog.name}</TableCell>
-              <TableCell>{catalog.description}</TableCell>
-              <TableCell>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button variant="outline" size="icon" className="mr-2">
-                      Editar
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogTitle>Editar Catálogo</DialogTitle>
-                    </DialogHeader>
-                    <div className="grid gap-4 py-4">
-                      <Input
-                        placeholder="Nome do Catálogo"
-                        value={editingCatalog?.name || ''}
-                        onChange={(e) => setEditingCatalog({ ...editingCatalog, name: e.target.value })}
-                      />
-                      <Input
-                        placeholder="Descrição"
-                        value={editingCatalog?.description || ''}
-                        onChange={(e) => setEditingCatalog({ ...editingCatalog, description: e.target.value })}
-                      />
-                      <Button onClick={updateCatalog}>Atualizar</Button>
-                    </div>
-                  </DialogContent>
-                </Dialog>
-                <Button variant="outline" size="icon" onClick={() => deleteCatalog(catalog.id)}>
-                  Excluir
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
     </div>
   )
 }
