3f65b53235eb0335a864ebc8062f45343611703b
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-31 13:23:20 -0300

    Feat: install tailwind

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,10 @@
 // react-frontend/src/App.js
-import React, { useEffect } from 'react';
+import React from 'react';
 
 function App() {
-  async function getData() {
-    try {
-      // Define a URL do backend
-      const apiUrl = process.env.REACT_APP_SUPABASE_URL
-        ? `${process.env.REACT_APP_SUPABASE_URL}/api/posts`
-        : 'http://localhost:3000/api/posts';
+ 
 
-      // Faz a chamada para o middleware no servidor Express
-      const res = await fetch(apiUrl);
-      if (!res.ok) {
-        console.error('Error fetching data:', res.statusText);
-        return;
-      }
-      const data = await res.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Failed to fetch data:', error);
-    }
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  return <div className="App">Hello, Middleware!</div>;
+  return <div className="bg-slate-700">Hello,</div>;
 }
 
 export default App;
