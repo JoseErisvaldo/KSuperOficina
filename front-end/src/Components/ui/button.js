b9d7e66a4d2056001e9af7416a155d95d0c15959
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-09-01 17:53:20 -0300

    style: adding display flex

--- front-end/src/Components/ui/button.js
+++ front-end/src/Components/ui/button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export function Button({ children, className, onClick, variant, size }) {
-  const baseStyle = 'px-4 py-2 rounded';
+  const baseStyle = 'px-4 py-2 rounded flex items-center justify-center gap-2';
   const variantStyle = variant === 'outline' ? 'border border-gray-400' : 'bg-blue-500 text-white';
   const sizeStyle = size === 'icon' ? 'p-2' : 'py-2 px-4';
 
